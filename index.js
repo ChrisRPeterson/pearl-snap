d3d4d61cb9449f3020ffb1ec871917296ab7daac
Christian Peterson <chrisbobpete@gmail.com>
2022-03-20 12:49:19 -0500

    Add dynamic event listener initialization

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const { token } = require("./config.json");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+// Initialize event listeners
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
+// Initialize slash commands
 client.commands = new Collection();
 const commandFiles = fs
   .readdirSync("./commands")
@@ -13,25 +28,5 @@ for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.data.name, command);
 }
-
-client.once("ready", () => {
-  console.log("Ready!");
-});
-
-client.on("interactionCreate", async (interaction) => {
-  const command = client.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
-  }
-});
 
 client.login(token);
